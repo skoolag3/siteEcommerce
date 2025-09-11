ffc606df4b698def45f675058a1fcc388fb9adae
skoolag3 <redgamesdzn@gmail.com>
2025-09-11 05:58:18 -0300

    base do home

--- projeto/settings.py
+++ projeto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
